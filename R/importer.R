ea653fb5e1a71432f591ac56ed5e30cb1f693472
pqstri <lucacarmisciano@hotmail.com>
2019-11-03 19:33:00 +0100

    bugfix for leading empty column

--- R/importer.R
+++ R/importer.R
@@ -1,6 +1,4 @@
 # Multi-line header excel importer
-library(tidyverse)
-library(readxl)
 
 #' Import Microsoft Excel file with multiple line header as R tibble
 #'
@@ -10,18 +8,22 @@ library(readxl)
 #' @return A tibble with a proper single line header
 #' @examples
 #' \dontrun{MLHimport("myExcelFileName.xlsx", lines = 3)}
-
 MLHimport <- function(path, lines, .args = list()) {
-  args_default <- c(path = path, .args)
-  args_db_call <- c(args_default, skip  = lines - 1)
-  args_header  <- c(args_default, n_max = lines - 1)
 
-  # Import the data
+  # Define arguments
+  args_default <- c(path = path, .args)
+  args_db_call <- c(args_default)
+  args_header  <- c(args_default, n_max = lines)
+
+  # Import the data and skip header
   db <- do.call(readxl::read_excel, args_db_call)
+  db <- db[-(1:lines - 1),]
 
   # Import the header
-  header <- do.call(readxl::read_excel, args_header) %>%
-    t() %>% data.frame() %>% rownames_to_column() %>% mutate_all(as.character)
+  header <- do.call(readxl::read_excel, args_header)
+  header <- header[-lines,]
+  header <- data.frame(t(header))
+  header <- dplyr::mutate_all(tibble::rownames_to_column(header), as.character)
 
   # Improve column readability
   names(header) <- paste("line", as.character(1:lines), sep = "_")
@@ -31,7 +33,7 @@ MLHimport <- function(path, lines, .args = list()) {
   header[placeholders, 1] <- NA
 
   # Fill placeholders NAs
-  header <- fill(header, 1)
+  header <- tidyr::fill(header, 1)
 
   # Fill other NAs
   for(line in 1:lines) {
@@ -40,17 +42,15 @@ MLHimport <- function(path, lines, .args = list()) {
 
     if ((line + 1) >= lines) break()
 
-    header <- header %>%
-      group_by(.dots = current_line) %>%
-      fill(next_line) %>%
-      ungroup()
+    header <- tidyr::fill(dplyr::group_by(header, .dots = current_line), next_line)
+    header <- dplyr::ungroup(header)
   }
 
   # Melt columns
   melted_col <- by(header, seq_len(nrow(header)), function(x) {
     paste((x), collapse = "_")})
 
-  col_names <- str_replace_all(melted_col, "_NA", "")
+  col_names <- stringr::str_replace_all(melted_col, "_NA", "")
 
   # Assign new column names
   names(db) <- col_names
